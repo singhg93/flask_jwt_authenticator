6337531e550159f1177234547fafe0cfb921d8e3
singhg93 <singhg93@student.douglascollege.ca>
2020-04-15 00:58:19 -0700

    added the pattern for password validation

--- jwtAuthenticator/schemas/schema_user.py
+++ jwtAuthenticator/schemas/schema_user.py
@@ -1,7 +1,9 @@
+# import the modules required for validating the user using json schema
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 from jsonschema.exceptions import SchemaError
 
+# define the user schema
 user_schema = {
     "type": "object",
     "properties": {
@@ -10,6 +12,7 @@ user_schema = {
         },
         "password": {
             "type": "string",
+            "pattern": "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
             "minLength": 8
         },
     },
@@ -17,13 +20,17 @@ user_schema = {
     "additionalProperties": False
 }
 
+# validate the user based data based on the json schema
 def validate_user(data):
     try:
+        # validate user data with the schema defined above
         validate(data, user_schema)
     except ValidationError as validate_error:
+        # if there was a validation error, return the error
         return {'ok': False, 'message': validate_error}
     except SchemaError as schema_error:
+        # if there was a schema error, return the error
         return {'ok': False, 'message': schema_error}
-
+    # if everything was valid and good, return the data with validation confirmation
     return {'ok': True, 'user_data': data}
 
