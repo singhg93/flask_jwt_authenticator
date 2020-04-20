033bbe4fcb07e0e18880d16fee29ec0530f0eb5b
singhg93 <singhg93@student.douglascollege.ca>
2020-04-20 11:41:31 -0700

    added tests for adding a user to the database

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,7 +1,14 @@
 import unittest
-from jwtAuthenticator.models import User
+from jwtAuthenticator.models import User, db
 
 class UserModelTestCase(unittest.TestCase):
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
     def test_password_setter(self):
         user = User(username="test", password="human")
@@ -21,3 +28,13 @@ class UserModelTestCase(unittest.TestCase):
         user1 = User(password="robot")
         user2 = User(password="robot")
         self.assertTrue(user1.password_hash != user2.password_hash)
+
+
+    def test_add_user(self):
+        user = User(username='test', password='human')
+        db.session.add(user)
+        db.session.commit()
+        user = User.query.filter_by(username='test').first()
+        self.assertIsNotNone(user)
+
+
